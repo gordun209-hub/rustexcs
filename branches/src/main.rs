8c588c769984267dbd3f2fd8431f205f0e24c5dc
gordun209 <ali-han80@hotmail.com>
2022-06-10 21:53:48 +0300

    add new ex

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -5,4 +5,33 @@ fn main() {
     } else {
         println!("condition was false");
     }
+    check_if_number_is_other_than_zero()
+}
+
+fn check_if_number_is_other_than_zero() {
+    let number = 3;
+    if number != 0 {
+        println!("number was something other than zero")
+    }
+    check_if_divisible_by_four_three_or_two()
+}
+
+fn check_if_divisible_by_four_three_or_two() {
+    let number = 6;
+    if number % 4 == 0 {
+        println!("number is divisilbe by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisilbe by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisilbe by 2");
+    } else {
+        println!("number is not divisible by 4,3 or 2");
+    }
+    proof_of_if_else_statements_are_expressions()
+}
+
+fn proof_of_if_else_statements_are_expressions() {
+    let condition = true;
+    let number = if condition { 5 } else { 2 };
+    println!("the value of number is {}", number)
 }
